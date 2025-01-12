bf7736ded0181410b2f7858197542ab347ebdb95
vansisto <vansisto@gmail.com>
2025-01-12 23:50:13 +0200

    C2: Implemented language changing

--- src/app/components/head/head.component.ts
+++ src/app/components/head/head.component.ts
@@ -1,26 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Button} from 'primeng/button';
 import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {DropdownModule} from 'primeng/dropdown';
+import {FormsModule} from '@angular/forms';
+import {Language} from '../../interfaces/language';
+import {Select} from 'primeng/select';
 
 @Component({
   selector: 'app-head',
   imports: [
     Button,
-    TranslatePipe
+    TranslatePipe,
+    DropdownModule,
+    FormsModule,
+    Select
   ],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
 export class HeadComponent {
+  languages: Language[] = [
+    {language: "UA"},
+    {language: "EN"}
+  ];
+  selectedLanguage: Language;
+
   constructor(private translate: TranslateService) {
-    const savedLanguage = localStorage.getItem('language') || 'uk';
-    this.translate.use(savedLanguage);
+    const storedLanguage = localStorage.getItem('language');
+    this.selectedLanguage = storedLanguage
+      ? (JSON.parse(storedLanguage) as Language)
+      : { language: 'UA' };
+
+    this.translate.use(this.selectedLanguage.language);
   }
 
-  changeLanguage(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    const language = selectElement.value;
-    this.translate.use(language);
-    localStorage.setItem('language', language);
+  changeLanguage(): void {
+    this.translate.use(this.selectedLanguage.language);
+    localStorage.setItem('language', JSON.stringify(this.selectedLanguage));
   }
 }
